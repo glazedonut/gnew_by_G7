b43e0a6b731bd95ef9ed6c8567e9af3670544d84
Paul Ouellette <oue.paul18@gmail.com>
2021-12-05 15:44:37 -0500

    Combine level 3 tests and add a level 4 test

--- tests/3a.sh
+++ tests/3a.sh
@@ -14,3 +14,19 @@ test_expect_success 'status shows added file' '
 	grep "A foo" out
 '
 
+test_expect_success 'cat works' '
+	gnew commit "add foo" &&
+	gnew cat HEAD foo >foo2 &&
+	diff foo foo2
+'
+
+test_expect_success 'log shows commit author and message' '
+	gnew log >out &&
+	grep G7 out &&
+	grep "add foo" out
+'
+
+test_expect_success 'heads shows current branch' '
+	gnew heads >out &&
+	grep "* main" out
+'
