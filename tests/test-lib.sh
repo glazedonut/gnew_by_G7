edd1273f9460c67e515eb1971162febeafb21deb
Paul Ouellette <oue.paul18@gmail.com>
2021-12-04 22:57:49 -0500

    Write some acceptance tests
    Note: the heads test is currently failing

--- tests/test-lib.sh
+++ tests/test-lib.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 # Gnew test library, based on the Git test framework.
 
+USER=G7
+
 red=$(tput bold; tput setaf 1)
 green=$(tput setaf 2)
 reset=$(tput sgr0)
 
-cd "$(mktemp -d)" || exit
-gnew init
+if test "$VERBOSE"
+then
+	exec 3>&1
+else
+	exec 3>/dev/null
+fi
+
+rm -rf testrun
+mkdir testrun && cd testrun || exit
+gnew init 2>&3 >&3
 
 test_expect_success () {
-	echo "test '$1': $2"
-	if eval "$2"; then
+	echo "test '$1': $2" >&3
+
+	if eval "$2" 2>&3 >&3
+	then
 		echo "${green}ok${reset} - $1"
 	else
 		echo "${red}FAIL${reset} - $1"
@@ -19,6 +31,6 @@ test_expect_success () {
 
 test_commit () {
 	echo "$1" >"$2" &&
-	gnew add "$2" &&
+	gnew add "$2" >/dev/null &&
 	gnew commit "$1"
 }
